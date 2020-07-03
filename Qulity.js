d1aee5c50c33bcc638c1782cef6b9b1e9bbef981
QSmally <github@qbot.eu>
2020-07-03 16:36:10 +0200

    Added base code

--- Qulity.js
+++ Qulity.js
@@ -1,2 +1,15 @@
-// Qulity has not yet started development.
-module.exports = () => {}
+
+/*
+    Qulity is built from the ground up by QSmally.
+    Qulity © 2020 by QSmally, all rights reserved.
+    Please report bugs by creating an issue on the repo.
+
+    Originally included in project QSmally/QDB.
+
+    Obviously the Node runtime is not created by myself,
+    they're created by their respective owners.
+    All rights are reserved.
+*/
+
+
+module.exports = {};
