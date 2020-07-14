50790998fb8ddaa34df019edc6d5efc8ef6fba15
QSmally <github@qbot.eu>
2020-07-14 12:27:53 +0200

    Iterate through files

--- Development/Gen/Main.js
+++ Development/Gen/Main.js
@@ -2,3 +2,16 @@
 /*
     Generates Qulity documentation
 */
+
+const Path = require("path");
+const FS   = require("fs");
+let Files  = [];
+
+FS.readdirSync("./lib/").forEach(Section => {
+    if (FS.statSync(`./lib/${Section}`).isDirectory())
+    FS.readdirSync(`./lib/${Section}`).forEach(File => Files.push(`./lib/${Section}/${File}`));
+    else Files.push(`./lib/${Section}`);
+});
+
+const Keys = Object.keys(require("../../Qulity"));
+Files = Files.filter(File => Keys.includes(Path.basename(File, ".js")));
