c1119ec91f865195f921d37fbd7602c1e385b95e
QSmally <github@qbot.eu>
2020-10-30 20:28:16 +0100

    Implement actual benchmarks

--- Test/Benchmark.js
+++ Test/Benchmark.js
@@ -1,15 +1,21 @@
 
 const Qulity = require("../../Qulity");
+
+console.log(`Benchmark:
+Fill instances with 1 million entries.
+Fetch 5 million over the same elements.`);
+
+const Coll = new Qulity.Collection();
+for (let i = 0; i < 1000000; i++) Coll.set(i, i);
+
+console.time("collection");
+for (let i = 0; i < 5000000; i++) Coll.get(i % 1000000);
+console.timeEnd("collection");
+
+
 const DS = new Qulity.DataStore();
+for (let i = 0; i < 1000000; i++) DS.set(i, i);
 
-DS.set("foo", {content: "bar"});
-DS.set("bar", {content: "roo"});
-DS.set("roo", {content: "foo"});
-
-console.time("Uncached");
-DS.resolve("foo");
-console.timeEnd("Uncached");
-
-console.time("Cached");
-DS.resolve("foo");
-console.timeEnd("Cached");
+console.time("datastore");
+for (let i = 0; i < 5000000; i++) DS.resolve(i % 1000000);
+console.timeEnd("datastore");
